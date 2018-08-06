9e028cb58e7d9a583bacd33c5c3ba76151ee70ef
zhelin-bina <zhe.lin@roche.com>
2018-08-06 13:30:59 -0700

    add module

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { Angulartics2Module } from 'angulartics2';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
+@NgModule({
+  imports: [
+    Angulartics2Module,
+  ],
+  declarations: [
+    HomeComponent,
+  ]
+})
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
